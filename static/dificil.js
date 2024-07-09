45eaeabe4099b48fa1773f4322f740f6e15f2a22
Cristian D. Serra <105523175+KristopherSerra@users.noreply.github.com>
2024-07-09 12:00:04 -0300

    Add files via upload

---
+++ static/dificil.js
@@ -0,0 +1,30 @@
+//Arreglo de Imagenes.
+let imgPreguntas = ["D1.png", "D2.jpg", "D3.png", "D4.png", "D5.png", "D6.png", "D7.jpg", "D8.png", "D9.png", "D10.jpg",];
+//Arreglo de Preguntas
+let preguntas = [
+    "1. ¿CUÁNTAS PELICULAS DEL MARVEL HAY A LA ACTUALIDAD (2023)?",
+    "2. ¿EN QUÉ AÑOS TUVO LUGAR EL EVENTO 'HOUSE OF M'?",
+    "3. ¿A QUÉ ANIMALES SE PARECEN LA RAZA ALIEN FLERKENS?",
+    "4. ¿CUÁL ES LA ÚNICA PELÍCULA DE MARVEL SIN ESCENA POST-CREDITOS?",
+    "5. ¿QUIÉN/ES HA/N DIRIGIDO MÁS PELÍCULAS DENTRO DEL MCU?",
+    "6. ¿DE QUÉ PERSONAJE DE 'TOP GUN' LLEVA EL NOMBRE DEL GATO DEL MS. MARVEL?",
+    "7. ¿CUAL ES EL DESTINO DE HULK EN 'WORLD WAR HULK'?",
+    "8. ¿QUE VILLANO DESATA LOS EVENTOS DE CIVIL WAR (2006) EN LOS COMICS?",
+    "9. ¿QUE LE PASA AL CAPITAN AMERICA DESPUES DE LA CIVIL WAR (2006)?",
+    "10. ¿CON QUIÉN HACEN UN TRATO PETER Y M.J. EN EL COMIC 'ONE MORE DAY'?",
+];
+//Arreglo de Opciones correctas.
+let correcta = [1, 0, 1, 3, 1, 2, 1, 3, 0, 1];
+
+//Arreglo de Opciones.
+let opciones = [];
+opciones.push(["30", "35", "40", "50"]);
+opciones.push(["2005/2006", "2006/2007", "2007/2008", "2008/2009"]);
+opciones.push(["Mapaches", "Gatos", "Perros", "Patos"]);
+opciones.push(["Iron-Man 3", "Capitan America - Civil War", "Black Panther", "Avengers: Endgame"]);
+opciones.push(["Jon Favreau", "Joe & Anthony Russo", "James Gunn", "Taika Waititi"]);
+opciones.push(["Viper", "Wolfman", "Goose", "Merlin"]);
+opciones.push(["Muere","Es Enterrado Vivo","Es Curado", "Se Vuelve Rey"]);
+opciones.push(["Speedball", "Surtur", "Magneto", "Nitro"]);
+opciones.push(["Es Asesinado", "Le Borran la Memoria", "Lo Torturan", "Es Liberado"]);
+opciones.push(["Dormammu", "Mefisto", "Black Hearth", "Zarathos"]);
---
+++ static/facil.js
@@ -0,0 +1,32 @@
+//Imagenes
+let imgPreguntas = ["F1.png", "F2.jpg", "F3.jpg", "F4.jpg", "F5.jpg", "F6.png", "F7.png", "F8.png", "F9.png", "F10.jpg",];
+
+//Preguntas
+let preguntas = [
+    "1. ¿EN QUE AÑO SE ESTRENO LA PRIMERA PELICULA DE IRON MAN?",
+    "2. ¿CÓMO SE LLAMA EL MARTILLO DE THOR?",
+    "3. ¿QUIÉN ES EL EL ACTOR QUE INTRERETA A HULK EN EL MCU?",
+    "4. ¿DE QUE ESTA HECHO EL ESCUDO DEL CAPITAN AMERICA?",
+    "5. ¿CUÁL ES LA PRIMER FAMILIA DE MARVEL EN LOS COMICS?",
+    "6. ¿CUÁL ES EL NOMBRE DEL PERSONAJE ANT-MAN(2015)?",
+    "7. ¿CUÁL ES EL VERDADERO NOMBRE DE LA PANTERA NEGRA?",
+    "8. ¿CÓMO ES EL NOMBRE REAL DEL PERSONAJE 'THE PUNISHER'?",
+    "9. ¿CUÁL ES EL RIVAL CLASICO DE SPIDERMAN EN LOS COMICS?",
+    "10. ¿CÓMO SON CLASIFICADOS LOS X-MEN?",
+];
+
+//Opciones
+let opciones = [];
+opciones.push(["2010", "2008", "2006", "2004"]);
+opciones.push(["Vanir", "Aesir", "Nørn", "Mjølnir"]);
+opciones.push(["Mark Ruffalo", "Lou Ferrigno", "Eric Bana", "Edward Norton"]);
+opciones.push(["Adamantium", "Titanium", "Carbonadium", "Vibranium"]);
+opciones.push(["Avengers", "Eternals", "Los 4 Fantasticos", "X - Men"]);
+opciones.push(["Scott Lang", "Mitchell Carson", "Hank Pym", "Darren Cross"]);
+opciones.push(["T'Chaka", "M'Baku", "N'Jobu", "T'Challa"]);
+opciones.push(["Scott Summers", "Frank Castle", "Reed Richards", "Luke Cage"]);
+opciones.push(["Duende Verde", "Dr. Doom", "Dormammu", "Abominacion"]);
+opciones.push(["InHumanos", "SuperHumanos", "Mutantes", "Skrull"]);
+
+//Respuestas Correctas
+let correcta = [1, 3, 0, 3, 2, 0, 3, 1, 0, 2];
